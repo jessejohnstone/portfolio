8666d3aa9c1d622af12d6cf6447e34a3b294ac34
JesseJohnstone <jessjohnstone203@gmail.com>
2026-09-24 19:30:52 +0300

    Update project

---
+++ backend/data/projects.js
@@ -0,0 +1,19 @@
+const projects = [
+  {
+    id: 1,
+    name: "AttachReady Kenya",
+    technology: "Next.js",
+  },
+  {
+    id: 2,
+    name: "TakaSmart Rewards",
+    technology: "React",
+  },
+  {
+    id: 3,
+    name: "Calm Corner",
+    technology: "JavaScript",
+  },
+];
+
+module.exports = projects;
